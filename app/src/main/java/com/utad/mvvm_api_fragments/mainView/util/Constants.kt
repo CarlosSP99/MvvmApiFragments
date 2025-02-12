4fafe67144670d3c897db9b439a57c2ca2ab33ab
Carlos Sánchez Pérez <csanchezp1999@gmail.com>
2025-02-12 09:55:47 +0100

    Organización carpetas y snackbars

--- app/src/main/java/com/utad/mvvm_api_fragments/mainView/util/Constants.kt
+++ app/src/main/java/com/utad/mvvm_api_fragments/mainView/util/Constants.kt
@@ -2,7 +2,7 @@ package com.utad.mvvm_api_fragments.mainView.util
 
 object Constants {
     const val BASE_URL="https://api.themoviedb.org/3/"
-    const val API_TOKEN="Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNGVlN2JhNmFlYmEwNTgwMmU3ZTYwYjU3ZTJmNmQyNCIsIm5iZiI6MTczNDQyMjI0Ni4xNzgsInN1YiI6IjY3NjEyZWU2ZTEwODZkOTRiZmI0ZjhkNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nO19o5CMw_wONiyFfK12rny_OoAYszCBG_kD8-qjK0c"
+    const val API_TOKEN=""
     const val BASE_URL_IMG = "http://image.tmdb.org/t/p/w200"
 
 }
