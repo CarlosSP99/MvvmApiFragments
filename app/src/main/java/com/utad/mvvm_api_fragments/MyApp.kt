b92c6e3727792410c890a3a659d7f46c9e7cbc11
Carlos Sánchez Pérez <csanchezp1999@gmail.com>
2025-02-12 09:55:18 +0100

    Organización carpetas y snackbars

---
+++ app/src/main/java/com/utad/mvvm_api_fragments/MyApp.kt
@@ -0,0 +1,9 @@
+package com.utad.mvvm_api_fragments
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltAndroidApp
+class MyApp : Application() {
+}
